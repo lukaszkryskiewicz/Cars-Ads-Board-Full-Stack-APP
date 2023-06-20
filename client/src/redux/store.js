c06eaccb6072249bd3776fdbd846e65d6fa65a06
Lukasz Kryskiewicz <lukasz.kryskiewicz@gmail.com>
2023-06-20 12:55:44 +0200

    Fix bug with ReactDevtools

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -15,8 +15,7 @@ const store = createStore(
   rootReducer,
   compose(
     applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f
   )
 );
-
 export default store;
